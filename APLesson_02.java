096ddd77f004c48c67afdaf76fc99934c90403f1
andreanai <11aji199@gmail.com>
2016-09-08 08:08:11 -0700

    Added Files

--- APLesson_02.java
+++ APLesson_02.java
@@ -6,5 +6,19 @@ public class APLesson_02
 		int num2 = 3;
 		
 		System.out.println(num1*num2);
+		
+		String name = "The White House";
+		String address = "1600 Pennsylvania Ave NW";
+		String city = "Washington, D.C.";
+		String zipcode = "20500";
+		
+		System.out.println(name + "\n" + address + "\n" + city + "\n" + zipcode);
+		
+		int l = 2;
+		int w = 3;
+		int h = 5;
+		int rectcubesurfacearea = 2*l*w + 2*l*h +2*h*w;
+		
+		System.out.println("The surface area of my rectangle is " + rectcubesurfacearea);
 	}
 }
